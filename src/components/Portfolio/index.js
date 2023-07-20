e11aba50c10b4724aae286b4b0b27e004cf64926
Jen Ryt <jenyjlin@gmail.com>
2023-07-20 16:26:22 -0700

    portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,10 +1,93 @@
 import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardActions,
+  CardMedia,
+  Button,
+  Typography,
+} from '@mui/material'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+import './index.scss'
+import portfolioData from '../../utils/portfolioData'
 
 const Portfolio = () => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3, // Set to 1 for vertical carousel
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1, // Set to 1 for vertical carousel
+    },
+  }
+
   return (
-    <div>
-      <h2>Portfolio</h2>
-      {/* Showcase your projects here */}
+    <div className="portfolio-container">
+      <div className="container portfolio-page">
+        <Carousel responsive={responsive} infinite>
+          {portfolioData.map((card, index) => (
+            <div key={index} className="cardWrapper">
+              <Card
+                // sx={{ maxWidth: 345 }}
+                style={{
+                  width: 345,
+                  height: '100%',
+                  borderRadius: 12,
+                  backgroundColor: '#c2b9a7',
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  height="60%"
+                  image={card.image}
+                  title={card.title}
+                  sx={{ objectFit: 'cover' }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h4" component="div">
+                    {card.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {card.content}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="large"
+                    href={card.url}
+                    target="_blank"
+                    style={{
+                      border: 'solid 1px #003262',
+                      borderRadius: 4,
+                      color: '#003262',
+                      transition: 'border-color 0.3s ease',
+                    }}
+                    onMouseEnter={(e) => {
+                      e.target.style.backgroundColor = '#003262'
+                      e.target.style.borderColor = '#003262'
+                      e.target.style.color = '#fff'
+                    }}
+                    onMouseLeave={(e) => {
+                      e.target.style.backgroundColor = 'transparent'
+                      e.target.style.borderColor = '#003262'
+                      e.target.style.color = '#003262'
+                    }}
+                  >
+                    Visit the Site
+                  </Button>
+                </CardActions>
+              </Card>
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </div>
   )
 }
---
+++ src/utils/portfolioData.js
@@ -0,0 +1,33 @@
+import TheSITE from '../assets/images/THE SITE you camp.png'
+import WanderList from '../assets/images/WanderList.png'
+import WeatherDashboard from '../assets/images/WeatherDashboard.png'
+import TimedQuiz from '../assets/images/TimedQuiz.png'
+
+const portfolioData = [
+  {
+    image: WanderList,
+    title: 'WanderList',
+    content: 'Content of Card',
+    url: 'https://wanderlist.herokuapp.com',
+  },
+  {
+    image: TheSITE,
+    title: 'The SITE you camp',
+    content: 'Content of Card',
+    url: 'https://kevrev.github.io/the-site/',
+  },
+  {
+    image: WeatherDashboard,
+    title: 'WeatherDashboard',
+    content: 'Content of Card',
+    url: 'https://jenryt.github.io/WeatherDashboard/',
+  },
+  {
+    image: TimedQuiz,
+    title: 'Timed Quiz Mini Game',
+    content: 'Content of Card',
+    url: 'https://jenryt.github.io/TimedQuiz/',
+  },
+]
+
+export default portfolioData

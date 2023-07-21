3d08cebda922885a42a102a05f7fd422663a3ca9
Jen Ryt <jenyjlin@gmail.com>
2023-07-21 09:24:31 -0700

    styling touch up , portfolio content inserted

--- src/utils/portfolioData.js
+++ src/utils/portfolioData.js
@@ -1,31 +1,148 @@
+import comingSoon from '../assets/images/comingSoon_imgPlaceholder.png'
 import TheSITE from '../assets/images/THE SITE you camp.png'
 import WanderList from '../assets/images/WanderList.png'
 import WeatherDashboard from '../assets/images/WeatherDashboard.png'
 import TimedQuiz from '../assets/images/TimedQuiz.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faJsSquare,
+  faNodeJs,
+  faHtml5,
+  faCss3,
+  faReact,
+  faSass,
+} from '@fortawesome/free-brands-svg-icons'
+// import {} from '@fortawesome/free-solid-svg-icons'
+
+// icons component
+const JS = () => {
+  return (
+    <FontAwesomeIcon
+      icon={faJsSquare}
+      color="#EFD81D"
+      className="anchor-icon"
+      style={{ padding: '3px' }}
+    />
+  )
+}
+
+const CSS3 = () => {
+  return (
+    <FontAwesomeIcon
+      icon={faCss3}
+      color="#28A4D9"
+      className="anchor-icon"
+      style={{ padding: '3px' }}
+    />
+  )
+}
+
+const HTML5 = () => {
+  return (
+    <FontAwesomeIcon
+      icon={faHtml5}
+      color="#F06529"
+      className="anchor-icon"
+      style={{ padding: '3px' }}
+    />
+  )
+}
+
+const NodeJS = () => {
+  return (
+    <FontAwesomeIcon
+      icon={faNodeJs}
+      color="#F06529"
+      className="anchor-icon"
+      style={{ padding: '3px' }}
+    />
+  )
+}
+
+const React = () => {
+  return (
+    <FontAwesomeIcon
+      icon={faReact}
+      color="#5ED4F4"
+      className="anchor-icon"
+      style={{ padding: '3px' }}
+    />
+  )
+}
+
+const Sass = () => {
+  return (
+    <FontAwesomeIcon
+      icon={faSass}
+      color="#CD6799"
+      className="anchor-icon"
+      style={{ padding: '3px' }}
+    />
+  )
+}
 
 const portfolioData = [
   {
+    image: comingSoon,
+    title: 'Engineering the Future : Jennifer Rytikoff',
+    content: (
+      <div>
+        <React />
+        <NodeJS />
+        <JS />
+        <HTML5 />
+        <Sass />
+        <CSS3 />
+      </div>
+    ),
+    url: 'https://www.google.com',
+  },
+  {
     image: WanderList,
     title: 'WanderList',
-    content: 'Content of Card',
+    content: (
+      <div>
+        <NodeJS />
+        <JS />
+        <CSS3 />
+      </div>
+    ),
     url: 'https://wanderlist.herokuapp.com',
   },
   {
     image: TheSITE,
     title: 'The SITE you camp',
-    content: 'Content of Card',
+    content: (
+      <div>
+        <JS />
+        <HTML5 />
+        <CSS3 />
+      </div>
+    ),
     url: 'https://kevrev.github.io/the-site/',
   },
   {
     image: WeatherDashboard,
     title: 'WeatherDashboard',
-    content: 'Content of Card',
+    content: (
+      <div>
+        <JS />
+        <HTML5 />
+        <CSS3 />
+      </div>
+    ),
     url: 'https://jenryt.github.io/WeatherDashboard/',
   },
   {
     image: TimedQuiz,
     title: 'Timed Quiz Mini Game',
-    content: 'Content of Card',
+    content: (
+      <div>
+        <JS />
+        <HTML5 />
+        <CSS3 />
+      </div>
+    ),
     url: 'https://jenryt.github.io/TimedQuiz/',
   },
 ]
